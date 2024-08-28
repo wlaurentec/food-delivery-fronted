99c007e066e1bc595024538b08eb2a796c1c3392
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-08-27 22:13:25 -0500

    import logo in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar/Navbar"
+
 const App = () => {
   return (
-    <div>
-      
+    <div className="app">
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import { assets } from "../../assets/assets"
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
