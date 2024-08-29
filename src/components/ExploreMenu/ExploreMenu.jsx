2d75da7fe969a1a23e71f8d7adc4bfb20ad66112
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-08-28 21:30:39 -0500

    add Fooditem strutucre

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -15,7 +15,7 @@ const ExploreMenu = ({category, setCategory}) => {
       <div className="explore-menu-list">
         {menu_list.map((item, index) => {
           return (
-            <div onClick={() => setCategory(prev=>prev === item.menu_name ? "All" : item.menu_name)} className="explore-menu-list-item" key={index}>
+            <div onClick={() => setCategory((prev)=>prev === item.menu_name ? "All" : item.menu_name)} className="explore-menu-list-item" key={index}>
               <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt="" />
               <p>{item.menu_name}</p>
             </div>
