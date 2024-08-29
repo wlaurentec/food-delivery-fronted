2edcb244ef3222b1be927cb06c4633c4ece3819d
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-08-28 20:33:22 -0500

    add menu select functionality

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,7 +1,8 @@
 import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category, setCategory}) => {
+  console.log(category)
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore our menu</h1>
@@ -14,13 +15,14 @@ const ExploreMenu = () => {
       <div className="explore-menu-list">
         {menu_list.map((item, index) => {
           return (
-            <div className="explore-menu-list-item" key={index}>
-              <img src={item.menu_image} alt="" />
+            <div onClick={() => setCategory(prev=>prev === item.menu_name ? "All" : item.menu_name)} className="explore-menu-list-item" key={index}>
+              <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt="" />
               <p>{item.menu_name}</p>
             </div>
           )
         })}
       </div>
+      <hr />
     </div>
   );
 };
