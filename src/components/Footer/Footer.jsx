b616fe04b6b33a5b4b9f430fa996f89696502241
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-08-29 20:20:48 -0500

    add footer jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import { assets } from "../../assets/assets";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+            laboriosam optio reprehenderit nostrum architecto? Quidem enim
+            animi, harum minima amet corporis labore nobis totam optio
+            asperiores veniam alias velit aperiam.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-345-6789</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 &copy; Tomato.com - All Right Reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
