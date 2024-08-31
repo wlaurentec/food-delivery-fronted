e2fc5347d245aa826697fbcfceb239aad9ca45e9
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-08-31 14:05:40 -0500

    add jsx Popup structure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { assets } from "../../assets/assets";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
@@ -44,7 +44,9 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>sign in</button>
+        <button
+          onClick={() => setShowLogin(true)}
+        >sign in</button>
       </div>
     </div>
   );
