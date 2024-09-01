c4a4e7f756244b95290d439ece5b51afd44afae4
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-09-01 13:13:56 -0500

    add cart styles

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,9 @@
-import { useContext } from "react"
-import { StoreContext } from "../../context/StoreContext"
-import "./Cart.css"
+import { useContext } from "react";
+import { StoreContext } from "../../context/StoreContext";
+import "./Cart.css";
 
 const Cart = () => {
-
-  const {cartItems, food_list, removeFromCart} = useContext(StoreContext)
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className="cart">
@@ -20,22 +19,58 @@ const Cart = () => {
         <br />
         <hr />
         {food_list.map((item, index) => {
-          if (cartItems[item._id]>0) {
+          if (cartItems[item._id] > 0) {
             return (
-              <div className="cart-items-title cart-items-item" key={index}>
-                <img src={item.image} alt="" />
-                <p>{item.name}</p>
-                <p>${item.price}</p>
-                <p>{cartItems[item._id]}</p>
-                <p>${item.price * cartItems[item._id]}</p>
-                <p>x</p>
+              <div key={index}>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
+                </div>
+                <hr />
               </div>
-            )
+            );
           }
         })}
       </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${0}</b>
+            </div>
+          </div>
+            <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+            <div>
+              <p>If you have a promo code, Enter it here</p>
+              <div className="cart-promocode-input">
+                <input type="text" placeholder="promo code"/>
+                <button>Submit</button>
+              </div>
+            </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
