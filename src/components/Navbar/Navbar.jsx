1e16c9999783a4df72802153825eaca7f3dacde8
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-09-01 10:16:27 -0500

    add cart page jsx struture

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to="/"><img src={assets.logo} alt="" className="logo" /></Link>
       <ul className="navbar-menu">
         <Link
           to="/"
@@ -41,7 +41,7 @@ const Navbar = ({ setShowLogin }) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to="/cart"><img src={assets.basket_icon} alt="" /></Link>
           <div className="dot"></div>
         </div>
         <button
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,39 @@
+import { useContext } from "react"
+import { StoreContext } from "../../context/StoreContext"
 import "./Cart.css"
 
 const Cart = () => {
+
+  const {cartItems, food_list, removeFromCart} = useContext(StoreContext)
+
   return (
-    <div>
-      
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id]>0) {
+            return (
+              <div className="cart-items-title cart-items-item" key={index}>
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>${item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>${item.price * cartItems[item._id]}</p>
+                <p>x</p>
+              </div>
+            )
+          }
+        })}
+      </div>
     </div>
   )
 }
