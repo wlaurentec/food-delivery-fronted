37d31caea6f38d1f12bfdf675fbeeeb50ac98b8b
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-09-11 20:57:01 -0500

    add loadCartData function in StoreContext

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -51,11 +51,23 @@ const StoreContextProvider = (props) => {
     setFoodList(response.data.data);
   };
 
+  const loadCartData = async (token) => {
+    const response = await axios.post(
+      `${url}/api/cart/get`,
+      {},
+      {
+        headers: { token },
+      }
+    );
+    setCartItems(response.data.cartData);
+  };
+
   useEffect(() => {
     async function loadData() {
       await fetchFoodList();
       if (localStorage.getItem("token")) {
         setToken(localStorage.getItem("token"));
+        await loadCartData(localStorage.getItem("token"));
       }
     }
 
